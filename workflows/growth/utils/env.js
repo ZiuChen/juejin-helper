8b900b18be00dfdc300ae1418776c5c75c59d0a2
Thund1r <thund1r@foxmail.com>
2022-09-26 02:22:27 +0800

    feat: 新增各成长任务开关，删除会引起误会的表情与评论文字，删除无用文件，修改README

--- workflows/growth/utils/env.js
+++ workflows/growth/utils/env.js
@@ -1,7 +1,21 @@
 const env = process.env || {};
 
+function getValue(name, defa) {
+  console.log(name + "结果", env[name]);
+  return env[name] ? JSON.parse(env[name]) : defa;
+}
+
 module.exports = {
   WORDS_API: env.WORDS_API || "i",
-  APPEND_EMOJI: env.APPEND_EMOJI || false,
-  ONLY_EMOJI: env.ONLY_EMOJI || false,
+  APPEND_EMOJI: getValue("APPEND_EMOJI", false),
+  ONLY_EMOJI: getValue("APPEND_EMOJI", false),
+  COLLECTARTICLE: getValue("ONLY_EMOJI", false),
+  DIGGARTICLE: getValue("DIGGARTICLE", true),
+  DIGGPIN: getValue("DIGGPIN", true),
+  COMMENTARTICLE: getValue("COMMENTARTICLE", true),
+  COMMENTPIN: getValue("COMMENTPIN", true),
+  FOLLOWAUTHOR: getValue("FOLLOWAUTHOR", true),
+  PUBLISHARTICLE: getValue("PUBLISHARTICLE", true),
+  PUBLISHPIN: getValue("PUBLISHPIN", true),
+  READARTICLE: getValue("READARTICLE", true)
 };
